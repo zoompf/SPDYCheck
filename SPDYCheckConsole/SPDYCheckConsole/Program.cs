deb0a302373f2ea11133c86dd50b6e6dacaaf2e7
Billy Hoffman <billy@zoompf.com>
2014-08-21 16:29:44 -0400

    New console app to help debug on weird servers

---
+++ SPDYCheckConsole/SPDYCheckConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Zoompf.SPDYAnalysis;
+
+namespace Zoompf.SPDYCheckConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SPDYResult result = SPDYChecker.Test("facebook.com", 443, 8000);
+
+            int x = 45;
+
+        }
+    }
+}
